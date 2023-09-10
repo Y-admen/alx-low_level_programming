8edd8b5f2658ce2b756e04fe70203fec71b9c623
Y-admen <yassminash305@gmail.com>
2023-09-10 09:36:49 -0700

    r

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -2,25 +2,26 @@
 /**
 *main -entry point
 *
-* describtion:  program that prints all possible different combinations of two digits.
+* describtion: print all possible different combinations of two digits.
 *
 *Return: always 0 (success)
 */
 int main(void)
 {
-	int i,x,l;
-	for ( i = 0; i <= 9; i++)
+	int i, x, l;
+
+	for (i = 0; i <= 8; i++)
 	{
-		for (x = i + 1; x < 10; x++)
+		for (x = i + 1; x < 9; x++)
 		{
-			for (l = i + 2; l < 9; l++)
-			putchar(i + 48);
-			putchar(x + 48);
-			putchar(l + 48);
-			if (i + x +l == 24)
-				continue;
-			putchar(',');
-			putchar(' ');
+			for (l = x + 2; l < 10; l++)
+				putchar(i + 48);
+				putchar(x + 48);
+				putchar(l + 48);
+				if (i + x + l == 24)
+					continue;
+				putchar(',');
+				putchar(' ');
 		}
 	}
 	putchar('\n');
--- 0x01-variables_if_else_while/try.c
+++ 0x01-variables_if_else_while/try.c
@@ -1,5 +1,3 @@
-#include <stdio.h>
-
 /**
  * main - Entry point
  *
@@ -9,20 +7,20 @@
  */
 int main(void)
 {
-	int i, j;
+	int i, x;
 
-	for (i = 0; i <= 8; i++)
+	for (i = 0; i <= 9; i++)
 	{
-		for (j = i + 1; j <= 9; j++)
-	{
-		putchar(i + '0');
-		putchar(j + '0');
-		if (i != 8 || j != 9)
+		for (x = i + 1; x < 10; x++)
 		{
-		putchar(',');
-		putchar(' ');
+
+			putchar(i + 48);
+			putchar(x + 48)
+			if (i + x == 17)
+				continue;
+			putchar(',');
+			putchar(' ');
 		}
-	}
 	}
 	putchar('\n');
 	return (0);
