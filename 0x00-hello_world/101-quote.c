0e9c76f3f082e1e60857f9fbf377ac60e35e9a74
Y-admen <yassminash305@gmail.com>
2023-09-10 02:24:35 -0700

    r

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,4 @@
-#include <unistd>
+#include <unistd.h>
 /**
  * main- entry point
  *
@@ -7,7 +7,7 @@
  * Return: always 1 (fail)
 */
 int main(void)
-{	char  *lin = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+{	char lin[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 	write(1, lin, 59);
 	return (1);
 }
