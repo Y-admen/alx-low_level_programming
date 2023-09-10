3b0fd1fe2a1d0e16c4ffe333010a969f78551979
Y-admen <yassminash305@gmail.com>
2023-09-10 01:12:41 -0700

    r

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,10 +6,10 @@
  * describtion: c program printing text using puts
  *
  * Return: always  0 (success)
-*/ 
+*/
 
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n");	
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
