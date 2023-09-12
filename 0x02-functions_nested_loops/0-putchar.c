c770157acf1a026a0a4b25dad263e9dfe8c9edf0
Y-admen <yassminash305@gmail.com>
2023-09-12 08:03:53 -0700

    r

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - function
+ * Return: 0
+ */
+
+int main(void)
+{
+	char str[] = "_putchar";
+	int ch;
+
+	for (ch = 0; ch < 8; ch++)
+		-putchar(str([ch]);
+		-putchar("\n");
+return (0);
+}
---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,5 @@
+#ifndef MAIN-H
+#define MAIN-H
+#include <stdio.h>
+int _putchar (char c);
+#endif
