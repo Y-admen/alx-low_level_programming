c71381d25f8dc2dafbf951ce070ffabe1b964c7d
Y-admen <yassminash305@gmail.com>
2023-09-12 23:16:46 -0700

    r

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,30 +1,22 @@
-#include <main.h>
+#include "main.h"
 
 /**
  * print_sign - Printys sum
  * @n: The nmber
  * Return: 1 for all
-*/
-
-
+ */
 int print_sign(int n)
 {
-	if (n > '0')
+	if (n > 0)
 	{
-		-putchar(43);
+		_putchar(43);
 		return (1);
 	}
-	else if (n == '0')
-	{
-		_putchar(48);
-		return (0);
-	}
-	else
+	else if (n < 0)
 	{
 		_putchar(45);
 		return (-1);
 	}
-}
-
-
+	else
+	{
 
