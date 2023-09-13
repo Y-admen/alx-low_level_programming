4af34419c413beb5e085c00a04365a2a10fea550
Y-admen <yassminash305@gmail.com>
2023-09-12 20:46:06 -0700

    r

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - print the alphabest 10 times
+ * Return: void here
+*/
+
+void print_alphabet_x10(void)
+{
+	char c;
+	int i = 0;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+		_putchar('\n');
+		i++;
+}
