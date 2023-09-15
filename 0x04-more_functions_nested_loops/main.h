2ea44b790ecae0cb2d96331c3e2efc03fd96e764
Y-admen <yassminash305@gmail.com>
2023-09-15 01:07:09 -0700

    r

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -10,6 +10,10 @@ void more_numbers(void);
 void print_most_numbers(void);
 void print_line(int n);
 void print_diagonal(int n);
+void print_square(int size);
+void print_triangle(int size);
+int mul(int a, int b);
+void print_number(int n);
 
 
 #endif
