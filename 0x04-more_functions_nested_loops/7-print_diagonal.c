64e2c72c925ef547788c62d55f0014e6d12a8286
Y-admen <yassminash305@gmail.com>
2023-09-15 01:03:20 -0700

    r

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -6,21 +6,26 @@
 void print_diagonal(int n)
 {
 	if (n <= 0)
-		_putchar('\n');
+	{
+	_putchar('\n');
+	}
 	else
 	{
-		int y, z;
+	int i, j;
 
-		for (y = 1; y <= n; y++)
-		{
-			for (z = 1; z <= y; z++)
-			{
-				_putchar(' ');
-			}
-			_putchar(92);
-			_putchar('\n');
-		}
+	for (i = 0; i < n; i++)
+	{
+	for (j = 0; j < n; j++)
+	{
+	if (j == i)
+	_putchar('\\');
+	else if (j < i)
+	_putchar(' ');
+	}
+	_putchar('\n');
+	}
 	}
 }
+
 
 
