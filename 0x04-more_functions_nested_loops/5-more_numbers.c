c4cfb53a65b01320f3fdb2d04102b2454af9c02e
Y-admen <yassminash305@gmail.com>
2023-09-15 00:02:30 -0700

    r

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -5,8 +5,18 @@
 void more_numbers(void)
 {
 	int a,b;
-	for (a == 0; a < 11; a ++)
+	for (a = 0; a < 11; a ++)
 	{
-		for (b == 0; b < 15; b ++)
-			_putchar(b + 48);
+		for (b = 0; b < 15; b ++)
+		{
+			if (b >= 10)
+			{
+				_putchar('1');
+				
+			}
+			_putchar(b % 10 + 48);
+				
+		}
+		_putchar('\n');
 	}
+}
