a1ee19fd6f433f076a99e993519833046ecd9061
Y-admen <yassminash305@gmail.com>
2023-09-18 06:51:42 -0700

    r

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * puts2 -  prints every other character of a string, starting with the first char
+ * puts2 -  prints every other character of a string
  *
  * @str: char parameter
  * Return: nothing
