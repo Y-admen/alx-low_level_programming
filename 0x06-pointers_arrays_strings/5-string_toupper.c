095a919192e0d1145e8b17487543b4f9582b79f2
Y-admen <yassminash305@gmail.com>
2023-09-21 02:52:08 -0700

    r

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * string_toupper - main function
+ *
+ * @n: String input
+ *
+ * Return: dest
+ */
+
+char *string_toupper(char *n)
+{
+	int i;
+	
+	i = 0;
+	while (n[i] != '\0')
+	{
+	if (n[i] >= 'a' && n[i] <= 'z')
+	n[i] = n[i] - 32;
+	i++;
+	}
+	return (n);
+}
+
