6b18be0bc3353a896d14b86c6f1fe0798b505075
Y-admen <yassminash305@gmail.com>
2023-09-25 09:09:35 -0700

    r

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+  * _strchr - main function
+  *
+  * @s: Function parameter
+  *
+  * @c: Function parameter
+  *
+  * Return: Always o.
+  */
+char *_strchr(char *s, char c)
+{
+	int k;
+
+	for (k = 0; s[k] >= '\0'; k++)
+	{
+	if (s[k] == c)
+	return (s + k);
+	}
+	return (0);
+}
+
