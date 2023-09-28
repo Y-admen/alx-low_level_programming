47e537c9d97c764b60d7ffc4516f7e930f751104
Y-admen <yassminash305@gmail.com>
2023-09-28 10:29:34 -0700

    r

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,29 +1,28 @@
 #include "main.h"
 
 /**
- * sqr - Makes possible to evaluate from 1 to n
+ * prime2 - Makes possible to evaluate from 1 to n
  * @a: same number as n
  * @b: number that iterates from 1 to n
  *
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
-int sqr(int a, int b)
+int prime2(int a, int b)
 {
-	if (b * b == a)
-		return (b);
-	else if (b * b > a)
-		return (-1);
-	return (sqr(a, b + 1));
+	if (a == b)
+		return (1);
+	else if (a % b == 0)
+		return (0);
+	return (prime2(a, b + 1));
 }
 /**
- * is_prime_number -  returns 1 if the input integer is a prime number
+ * is_prime_number - checks if a number is prime
+ * @n: Number Integer
  *
- * @n: int value
- *
- * Return: 1 if prime
- *      0 if not
-*/
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
 int is_prime_number(int n)
 {
 	if (n <= 1)
