3519c1058e7a64de5845ed8054cd7a9eb46b5b35
Y-admen <yassminash305@gmail.com>
2023-09-28 10:28:13 -0700

    r

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -24,10 +24,11 @@ int sqr(int a, int b)
  * Return: 1 if prime
  *      0 if not
 */
-
 int is_prime_number(int n)
 {
-	return (sqr(n, 1));
+	if (n <= 1)
+		return (0);
+	return (prime2(n, 2));
 }
 
 
