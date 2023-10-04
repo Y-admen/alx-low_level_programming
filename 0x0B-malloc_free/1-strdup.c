1e37f801cf31b746ba674574650a114bc962033e
Y-admen <yassminash305@gmail.com>
2023-10-04 00:16:33 -0700

    r

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -12,7 +12,7 @@ char *_strdup(char *str)
 	int len = 0, i;
 	char *s;
 
-	if (str == NULL)
+	if (str == 0)
 		return (null);
 	while (str[len] != '\0')
 	{
@@ -21,7 +21,7 @@ char *_strdup(char *str)
 	s = malloc(len * sizeof(str) + 1);/*space for the null '\0'*/
 	if (s == 0)
 	{
-		return (Null);
+		return (0);
 	}
 	else
 	{
