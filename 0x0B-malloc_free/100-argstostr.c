6ef81796c30d32b6c7a7258fe1cf4e4c7280020a
Y-admen <yassminash305@gmail.com>
2023-10-04 11:55:26 -0700

    r

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -23,7 +23,7 @@ char *argstostr(int ac, char **av)
 	int i = 0, nc = 0, j = 0, cmpt = 0;
 	char *s;
 
-	if (ac == 0 || av == NULL)
+	if (ac == 0 || av == 0)
 		return (NULL);
 	for ( ; i < ac; nc++, i++)
 		nC += _stlen(av[i]);
