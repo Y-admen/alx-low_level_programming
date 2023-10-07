4e2dc169f793c0f34d68ae18ecf613a637fafcd4
Y-admen <yassminash305@gmail.com>
2023-10-07 07:07:28 -0700

    r

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -10,17 +10,17 @@ int main(int argc, char *argv[])
 {
 	int mull, val1, val2;
 
-	if (argc < 3 || argc > 3)
-	{
-		printf("Error\n");
-		return (1);
-	}
-	else
+	if (argc == 3)
 	{
 		val1 = atoi(argv[1]);
 		val2 = atoi(argv[2]);
 		mull = val1 * val2;
 		printf("%d\n", mull);
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
 	}
 	return (0);
 }
