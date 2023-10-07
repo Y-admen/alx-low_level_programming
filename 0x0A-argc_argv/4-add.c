d595ab427f557fc2550b3e2d5449afddaee2a967
Y-admen <yassminash305@gmail.com>
2023-10-07 07:47:15 -0700

    r

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,1 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+int main(int argc, char *argv[]) 
+{
+	int i, sum = 0;
+
+	if (argc == 1)
+		printf("0\n");
+	else 
+	{
+		for (i = 1; i < argc; i++) 
+		{
+			if (argv[i] >= '0' || argv[i] <= '9') 
+			{
+				sum += atoi(argv[i]);
+			} 
+			else 
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+    		printf("%d\n", sum);
+	}
+	return 0;
+}
