ad81c12ec5e7ae3d480d10b348a50c379bad1259
Y-admen <yassminash305@gmail.com>
2023-10-07 07:58:47 -0700

    r

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,27 +1,24 @@
-#include <stdio.h>
+#include<stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char *argv[]) 
+/*
+*main - it all starts here
+* Qargc: the number of arguments
+* Qargv: array of pointers to arguments
+* Return: Always 0.
+*/
+int main(int argc, char *argv[])
 {
-	int i, sum = 0;
+	int sum = 0;
+	char *c;
 
-	if (argc == 1)
-		printf("0\n");
-	else 
+	while (--argc)
 	{
-		for (i = 1; i < argc; i++) 
-		{
-			if (argv[i] >= '0' || argv[i] <= '9') 
-			{
-				sum += atoi(argv[i]);
-			} 
-			else 
-			{
-				printf("Error\n");
-				return (1);
-			}
-		}
-    		printf("%d\n", sum);
+		for (c = argv[argc]; *C; C++)
+			if (*c < '0' || *c > '9')
+				return (printf("Error\n"), 1);
+		sum += atoi(argv[argc]);
 	}
-	return 0;
+	printf("%d\n", sum);
+	return (0);
 }
