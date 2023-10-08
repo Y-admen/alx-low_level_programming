4bdd4f1ef69c0a85aed9d23a023176fb9a1d0df8
Y-admen <yassminash305@gmail.com>
2023-10-08 01:34:13 -0700

    r

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -6,30 +6,23 @@
 * @n: max bytes to use
 * Return: s
 */
-char *_memset(char *s, char b, unsigned int n)
-{
-	char *ptr;
-	ptr = s;
-
-	while (n--)
-		*s++ = b;
-	return (ptr);
-}
-/*
-* *_calloc - allocates memory for an array, using malloc
-* @nmemb: array length
-* @size: size of each element
-* Return: pointer
-*/
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	void *m;
+	char *ptr;
+	unsigned int count;
 
-	if (size == 0 || nmemb == 0)
+	if (nmemb == 0 || size == 0)
 		return (NULL);
-	m = malloc(sizeof(int) * nmemb);
-	if (m == 0)
+	ptr = malloc(nmemb * size);
+	if (ptr == NULL)
+	{
 		return (NULL);
-	_memset(m, 0, sizeof (int) * nmemb);
-	return (m);
+	}
+	count = 0;
+	while (count < nmemb * size)
+	{
+		ptr[count] = 0;
+		count++;
+	}
+	return (ptr);
 }
