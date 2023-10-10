338d3f037f4bd290a766c6d925f3e428344e4869
Y-admen <yassminash305@gmail.com>
2023-10-10 08:32:59 -0700

    r

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -6,12 +6,13 @@
  *@name:dog name
  *@age:dog age
  *@owner:dog owner
+ *Return: void
 */
 
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	if (d)
+	if (d != 0)
 	{
 		d->name = name;
 		d->age = age;
