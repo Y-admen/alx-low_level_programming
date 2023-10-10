f448e0c86bcbc048ff44381514acbe870ceda5fb
Y-admen <yassminash305@gmail.com>
2023-10-10 07:30:10 -0700

    r

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+/*guard of header file*/
+
+/**
+ * struct dog - stuct dog
+ * @name:dog name
+ * @age: dog age
+ * @owner: owner name
+ * Describtion: Define a new type struct dog
+*/
+
+struct dog {
+	char* name;
+	float age;
+	char* owner;
+}
+#endif
