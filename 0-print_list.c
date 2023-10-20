5af9099f51835397414f54fc20725b45ec66c5b0
Y-admen <yassminash305@gmail.com>
2023-10-19 17:11:15 -0700

    test

---
+++ 0-print_list.c
@@ -0,0 +1,24 @@
+#include "list.h"
+
+/***/
+
+size_t print_list(const list_t *h)
+{
+	size_t count;
+
+	while(h)
+	{
+		if (h -> str == NULL)
+			printf("[0] (nil)\n";
+		else
+			printf("[%d] %s\n",h -> len ,h -> str);
+
+
+		count++;
+		h = h -> next;
+		
+	}
+	return (count);
+}
+
+
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -4,6 +4,11 @@
 #include <stdio.h>
 #include <stdarg.h>
 
+typedef struct format
+{
+	char *op;
+	void (*f)(va_list all);
+} print;
 
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
