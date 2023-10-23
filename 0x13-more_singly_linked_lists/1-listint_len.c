f7f06d0e2cf85aa62cf67347cd139f14530311b1
root <root@DESKTOP-8A28FL7>
2023-10-23 13:30:19 +0300

    test

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,10 +1,13 @@
+#include"lists.h"
+
+
 size_t listint_len(const listint_t *h)
 {
   size_t count = 0;
 
   while (h != NULL) {
     count++;
-    h = h->next;
+    h = h-> next;
   }
 
   return count;
