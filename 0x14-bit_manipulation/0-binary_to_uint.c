dfc635e84a28cfdda1e450ee2630e1cd19441925
root <root@DESKTOP-8A28FL7>
2023-10-27 12:52:49 +0200

    bit

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -10,19 +10,29 @@
 
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int base_2, decimal = 0;
-	int len;
+	unsigned int ui;
+	int len, base_two;
 
 	if (!b)
 		return (0);
-	for (len = 0 ; b[len] != '\0'; len++)
+
+	ui = 0;
+
+	for (len = 0; b[len] != '\0'; len++)
 		;
-	for (len--, base_2 = 1; len >= 0; len--, base_2 *= 2)
+
+	for (len--, base_two = 1; len >= 0; len--, base_two *= 2)
 	{
-		if (b[len] != 0 && b[len] != 1)
+		if (b[len] != '0' && b[len] != '1')
+		{
 			return (0);
-		if (b[len] == 1)
-			decimal += base_2;
+		}
+
+		if (b[len] & 1)
+		{
+			ui += base_two;
+		}
 	}
-	return (decimal);
+
+	return (ui);
 }
