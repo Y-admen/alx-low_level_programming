275fa51427fd2ce1be8110a37ea38466f85dceed
root <root@DESKTOP-8A28FL7>
2023-10-27 07:33:01 +0200

    bit

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -9,17 +9,19 @@
 
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int base_2, pow, decimal;
+	unsigned int base_2, pow, decimal = 0;
 	int len;
 
 	if (!b)
 		return (0);
-	for (len = 0 ; b[len] != "\0"; len++)
+	for (len = 0 ; b[len] != '\0'; len++)
 		;
 	for (len--, pow = 0; len; pow++, base_2 = 2 ^ pow)
+	{
 		if (b[len] != 0 && b[len] != 1)
 			return (0);
 		if (b[len] == 1)
 			decimal += base_2;
+	}
 	return (decimal);
 }
