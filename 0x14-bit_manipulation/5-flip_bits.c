81e521113d92724eae4719aa77d823b460c1d7f1
root <root@DESKTOP-8A28FL7>
2023-10-27 12:35:12 +0200

    bit

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -8,19 +8,17 @@
  * Return: The necessary number of bits to flip to get from n to m
  */
 
-unsigned int flip_bits(unsigned long int n, unsigned long int m) 
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
 	unsigned int count = 0;
 	unsigned long int xor;
 
 	xor = n ^ m;
-  // Count the number of set bits in the XOR result.
-	unsigned int count = 0;
-	while (xor > 0) 
+  /*Count the number of set bits in the XOR result*/
+	while (xor > 0)
 	{
-		if (xor & 1) 
+		if (xor & 1)
 			count++;
-    
 	xor >>= 1;
 	}
 
