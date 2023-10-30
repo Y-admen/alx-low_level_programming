ff3836b7bf5f5e6dc5a97d3681384a14b061b6a9
root <root@DESKTOP-8A28FL7>
2023-10-30 12:59:21 +0200

    file i/o

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * read_textfile -  function that reads a text file and prints it to stdout
+ * @filename :filename
+ * @letters: num of letters
+ * Return: wr
+*/
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	ssize_t op, re, wr;
+	char *ptr;
+
+	if (filename == NULL || letters == 0)
+		return (0);
+	ptr = malloc(sizeof(char) * letters);
+	if (!ptr)
+		return (0);
+	op = open(filename, O_RDONLY);
+	re = read(op, ptr, letters);
+	wr = write(re, ptr, letters);
+
+	if (op == -1 || re == -1 || wr == -1)
+	{
+		free(ptr);
+		return (0);
+	}
+	free(ptr);
+	return (wr);
+}
