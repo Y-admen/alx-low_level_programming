7b1903c649d9ad3d3ec4035c7e4ed3baffd702d7
root <root@DESKTOP-8A28FL7>
2023-10-30 16:01:00 +0200

    file i/o

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -17,7 +17,7 @@ int create_file(const char *filename, char *text_content)
 	op = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
 	if (text_content == NULL)
 		text_content = "";
-	while (text_content[len] != "\0")
+	while (text_content[len])
 	{
 		len++;
 	}
