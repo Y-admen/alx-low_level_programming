df5e9acc4d48c922c29c3ff6e5053c2fc000079d
root <root@DESKTOP-8A28FL7>
2023-10-30 16:32:11 +0200

    file i/o

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -6,13 +6,14 @@
  * @text_content: content to be written
  * Return: i on success, -1 fail
 */
+
 int append_text_to_file(const char *filename, char *text_content)
 {
 	int op, wr, len = 0;
 
 	if (!filename)
 		return (-1);
-	op = open(filename, O_WRONLY, O_APPEND);
+	op = open(filename, O_WRONLY|O_APPEND);
 	while (text_content[len])
 		len++;
 	if (text_content == NULL)
