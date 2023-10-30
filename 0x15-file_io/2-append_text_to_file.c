97149f512ed8133e2fda5e1cfe579270325dc913
root <root@DESKTOP-8A28FL7>
2023-10-30 16:25:32 +0200

    file i/o

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * append_yext_to_file -  appends text at the end of a file
+ * @filename: filename
+ * @text_content: content to be written
+ * Return: i on success, -1 fail
+*/
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int op, wr, len = 0;
+
+	if (!filename)
+		return (-1);
+	op = open(filename, O_WRONLY, O_APPEND);
+	while (text_content[len])
+		len++;
+	if (text_content)
+		wr = write(op, text_content, len);
+	if (op == -1 || wr == -1)
+	return (-1);
+
+	close(op);
+	return (1);
+
+}
