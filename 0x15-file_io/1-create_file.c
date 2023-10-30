109c27534194f1cf869ac62c9b73445b2083cb89
root <root@DESKTOP-8A28FL7>
2023-10-30 15:55:01 +0200

    file i/o

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,5 +1,10 @@
 #include "main.h"
-/***
+/**
+ * creat_file -  creates a file.
+ * @text_content: text to be written
+ * @filename: file name
+ * Return: 1 on success
+ * -1 fail
  */
 
 int create_file(const char *filename, char *text_content)
@@ -9,8 +14,8 @@ int create_file(const char *filename, char *text_content)
 	if (!filename)
 		return (-1);
 
-	op = open(filename, O_RDWR, O_CREAT, O_TRUNC);
-	while (text_content[len])
+	op = open(filename, O_RDWR, O_CREAT, O_TRUNC, 0600);
+	while (text_content[len] != "\0")
 	{
 		len++;
 	}
