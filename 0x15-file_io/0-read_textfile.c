b44ab3e644904194ad3c47a5650b29a66a2dcb27
root <root@DESKTOP-8A28FL7>
2023-10-30 13:17:15 +0200

    file i/o

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -19,15 +19,14 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		return (0);
 	op = open(filename, O_RDONLY);
 	re = read(op, ptr, letters);
-	close(op);
-	wr = write(re, ptr, re);
+	wr = write(STDOUT_FILENO, ptr, re);
 
-	if (op == -1 || re == -1 || wr == -1 || wr != re)
+	if (op == -1 || re == -1 || wr == -1)
 	{
 		free(ptr);
 		return (0);
 	}
-	
+	close(op);
 	free(ptr);
 	return (wr);
 }
