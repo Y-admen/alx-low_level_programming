f75af2d4a373b362413a995f2ffc134107557af8
Y-admin <yassminash305@gmail.com>
2023-12-23 01:42:01 +0200

    hash

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,29 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - Create the hash table.
+ * @size: Size of the table.
+ *
+ * Return: The hash table.
+ */
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	unsigned int i = 0;
+	hash_table_t *table;
+
+	if (!size)
+	return (NULL); /* Zero is not a valid argument */
+	table = malloc(sizeof(hash_node_t));
+	if (!table)
+	return (NULL); /* Allocation failed */
+	table->size = size;
+	table->array = malloc(sizeof(hash_node_t) * size);
+	if (!array)
+	return (NULL); /* Allocation failed */
+	for (i = 0; i < size; ++i)
+		table->array[i] = NULL;
+	return (table);
+
+
+}
