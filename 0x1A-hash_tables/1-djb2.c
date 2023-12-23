e0f1ea6a2c7070b38c6cfb380e3a21ec9d60c785
Y-admin <yassminash305@gmail.com>
2023-12-23 03:09:51 +0200

    2

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -1,8 +1,8 @@
-#on
+#include "hash_tables.h"
 
 /**
- * hash_djb2 - implementation of the djb2 algorithm
- * @str: string used to generate hash value
+ * hash_djb2 -  Hash function that implement djb2 algorithm
+ * @str: Key to aplied the transformation.
  *
  * Return: hash value
  */
