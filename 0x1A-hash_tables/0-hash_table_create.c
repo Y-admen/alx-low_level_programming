d76173b2bbaff8d9ca695ca296b69219e21c912f
Y-admin <yassminash305@gmail.com>
2023-12-23 02:14:28 +0200

    table

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -18,7 +18,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	if (!table)
 	return (NULL); /* Allocation failed */
 	table->size = size;
-	table->array = malloc(sizeof(hash_node_t*) * size);
+	table->array = malloc(sizeof(hash_node_t *) * size);
 	if (!table->array)
 	return (NULL); /* Allocation failed */
 	for (i = 0; i < size; i++)
